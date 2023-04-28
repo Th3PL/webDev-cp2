5b783def05d60a7cf222ada696bf4add2bc2ed86
Th3PL <plucas2013@gmail.com>
2023-04-28 14:10:02 -0300

    v6

--- scipt.js
+++ scipt.js
@@ -1,11 +1,11 @@
 document.getElementById("btn-padrao").addEventListener("click", function(){
-    document.querySelector("body").setAttribute("class", "padrao");
+    document.querySelector("body", "header", "div").setAttribute("class", "padrao");
 })
 
 document.getElementById("btn-dark").addEventListener("click", function(){
-    document.querySelector("body").setAttribute("class", "dark");
+    document.querySelector("body", "*", "div").setAttribute("class", "dark");
 })
 
 document.getElementById("btn-green").addEventListener("click", function(){
-    document.querySelector("body").setAttribute("class", "green");
+    document.querySelector("body", "header", "div").setAttribute("class", "green");
 })
