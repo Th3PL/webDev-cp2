812d41a2d94fa84d721c5667a6e0867b12ee5259
Th3PL <plucas2013@gmail.com>
2023-04-28 17:08:45 -0300

    v9

---
+++ questions.js
@@ -0,0 +1,66 @@
+export default [
+    {
+      question: "Qual dos nome abaixo NÃO é um tipo de vinho?",
+      answers: [
+        { option: "Molhado", correct: true },
+        { option: "Branco", correct: false },
+        { option: "Suave", correct: false },
+      ],
+    },
+    {
+      question: "Vinho branco é feio APENAS por uvas brancas?",
+      answers: [
+        { option: "Claro", correct: false },
+        { option: "Não", correct: true },
+      ],
+    },
+    {
+      question: "Solos Ruins produzem vinhos melhores?",
+      answers: [
+        { option: "Claro que não", correct: false },
+        { option: "Por incrível que pareça sim", correct: true },
+      ],
+    },
+    {
+      question: "Vinho tinto é melhor para o inverno?",
+      answers: [
+        { option: "Sim", correct: false },
+        { option: "Não", correct: true },
+      ],
+    },
+    {
+      question: "O especialista em vinho é chamado de:",
+      answers: [
+        { option: "Maestro", correct: false },
+        { option: "Somelier", correct: true },
+        { option: "Vinhólogo", correct: false },
+      ],
+    },
+    {
+      question: "Famoso no mundo inteiro o champagne é um vinho de origem: ",
+      answers: [
+        { option: "Americana", correct: false },
+        { option: "Francesa", correct: true },
+        { option: "Italiana", correct: false },
+      ],
+    },
+    {
+      question: "Todas são uvas de origem francesa, exceto:",
+      answers: [
+        { option: "Riesling", correct: true },
+        { option: "Cabernet Franc.", correct: false },
+        { option: "Gamay", correct: false },
+      ],
+    },
+    {
+      question: "São acessórios relacionados a vinho, exceto:",
+      answers: [
+        { option: "Colmeia", correct: false },
+        { option: "Decantador", correct: false },
+        { option: "Rótulo", correct: true },
+      ],
+    },
+    
+
+
+  ];
